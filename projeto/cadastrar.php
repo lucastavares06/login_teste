8a1e2f4e20a4043be914487d440311ecd864ff4b
lucastavares06 <146768406+lucastavares06@users.noreply.github.com>
2023-12-19 19:01:54 -0300

    Update cadastrar.php

--- projeto/cadastrar.php
+++ projeto/cadastrar.php
@@ -5,29 +5,25 @@
                 <div class="card-body">
                     <h2 class="card-title text-center mb-4 border-bottom pb-3">Cadastrar</h2>
 
-                    <?php
-                    if (isset($_GET['erro'])) {
-                        $erro = urldecode($_GET['erro']);
-                    ?>
-                        <div class="alert alert-danger" role="alert">
-                            <?php echo $erro; ?>
-                        </div>
-                    <?php
-                    }
-
-                    if (isset($_GET['mensagem'])) {
-                        $mensagem = urldecode($_GET['mensagem']);
-                    ?>
-                        <div class="alert alert-success" role="alert">
-                            <?php echo $mensagem; ?>
-                            <br>
-                            <a href="?pagina=login" class="alert-link">Clique aqui para fazer login</a>
-                        </div>
-                    <?php
-                    }
-                    ?>
-
                     <form action="public/processa_cadastrar.php" method="post">
+
+                        <?php if (isset($_GET['erro'])) : ?>
+                            <div class="alert alert-danger" role="alert">
+                                <?= htmlspecialchars(urldecode($_GET['erro'])); ?>
+                            </div>
+                        <?php endif; ?>
+
+                        <?php if (isset($_GET['mensagem'])) : ?>
+                            <?php
+                            $mensagem = urldecode($_GET['mensagem']);
+                            ?>
+                            <div class="alert alert-success" role="alert">
+                                <?= $mensagem; ?>
+                                <br>
+                                <a href="?pagina=login" class="alert-link">Clique aqui para fazer login</a>
+                            </div>
+                        <?php endif; ?>
+                        
                         <div class="mb-3">
                             <label for="nome" class="form-label">Nome Completo:</label>
                             <div class="input-group">
