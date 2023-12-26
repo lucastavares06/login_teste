bb892a01b2e242800837ef1e96c08fe33bc96ca1
lucastavares06 <146768406+lucastavares06@users.noreply.github.com>
2023-12-26 18:28:28 -0300

    Update processa_cadastrar.php

--- projeto/public/processa_cadastrar.php
+++ projeto/public/processa_cadastrar.php
@@ -3,17 +3,20 @@ include '../config/config.php';
 
 // Verifique se o formulário foi enviado
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Obtenha os dados do formulário
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $usuario = $_POST['usuario'];
-    $senha = $_POST['senha'];
-
-    // Verifique se o usuário já existe no banco de dados
-    $verificarUsuario = "SELECT * FROM usuario WHERE usuario = :usuario";
-    $verificarEmail = "SELECT * FROM usuario WHERE email = :email";
-
     try {
+        // Obtenha os dados do formulário
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $usuario = $_POST['usuario'];
+        $senha = $_POST['senha'];
+
+        // Verifique se o usuário já existe no banco de dados
+        $verificarUsuario = "SELECT * FROM usuario WHERE usuario = :usuario";
+        $verificarEmail = "SELECT * FROM usuario WHERE email = :email";
+
+        // Iniciar transação
+        $conexao->beginTransaction();
+
         // Verificar se o usuário já existe
         $stmtUsuario = $conexao->prepare($verificarUsuario);
         $stmtUsuario->bindParam(':usuario', $usuario);
@@ -25,39 +28,37 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmtEmail->execute();
 
         if ($stmtUsuario->rowCount() > 0) {
-            $erro = urlencode("Este nome de usuário já está em uso. Escolha outro.");
-            header("Location: ../index.php?pagina=cadastrar&erro=$erro");
-            exit();
+            throw new Exception("Este nome de usuário já está em uso. Escolha outro.");
         } elseif ($stmtEmail->rowCount() > 0) {
-            $erro = urlencode("Este e-mail já está em uso. Escolha outro.");
-            header("Location: ../index.php?pagina=cadastrar&erro=$erro");
+            throw new Exception("Este e-mail já está em uso. Escolha outro.");
+        }
+
+        // Insira o novo usuário no banco de dados
+        $inserirUsuario = "INSERT INTO usuario (nome, email, usuario, senha) VALUES (:nome, :email, :usuario, :senha)";
+
+        $stmt = $conexao->prepare($inserirUsuario);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':usuario', $usuario);
+
+        // Recomenda-se usar hashes seguros para senhas
+        $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
+        $stmt->bindParam(':senha', $senhaHash);
+
+        // Execute a consulta
+        if ($stmt->execute()) {
+            // Commit da transação se tudo ocorreu bem
+            $conexao->commit();
+            $mensagem = urlencode("Cadastro realizado com sucesso, $nome!");
+            header("Location: ../index.php?pagina=cadastrar&mensagem=$mensagem");
             exit();
         } else {
-            // Insira o novo usuário no banco de dados
-            $inserirUsuario = "INSERT INTO usuario (nome, email, usuario, senha) VALUES (:nome, :email, :usuario, :senha)";
-
-            $stmt = $conexao->prepare($inserirUsuario);
-            $stmt->bindParam(':nome', $nome);
-            $stmt->bindParam(':email', $email);
-            $stmt->bindParam(':usuario', $usuario);
-
-            // Recomenda-se usar hashes seguros para senhas
-            $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
-            $stmt->bindParam(':senha', $senhaHash);
-
-            // Execute a consulta
-            if ($stmt->execute()) {
-                $mensagem = urlencode("Cadastro realizado com sucesso, $nome!");
-                header("Location: ../index.php?pagina=cadastrar&mensagem=$mensagem");
-                exit();
-            } else {
-                $erro = urlencode("Erro durante o cadastro");
-                header("Location: ../index.php?pagina=cadastrar&erro=$erro");
-                exit();
-            }
+            throw new Exception("Erro durante o cadastro");
         }
-    } catch (PDOException $e) {
-        $erro = urlencode("Erro na consulta: " . $e->getMessage());
+    } catch (Exception $e) {
+        // Rollback em caso de erro
+        $conexao->rollBack();
+        $erro = urlencode("Erro durante o cadastro: " . $e->getMessage());
         header("Location: ../index.php?pagina=cadastrar&erro=$erro");
         exit();
     }
